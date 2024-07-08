cd8cceeb2a7facc1e5d1962b02370de061baeabd
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2024-07-08 19:37:45 +0300

    Small changes

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,8 +1,14 @@
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
+import { z } from 'zod'
+const env = z
+  .object({
+    DRIZZLE_DATABASE_URL: z.string(),
+  })
+  .parse(process.env)
 
 const client = createClient({
-  url: process.env.DRIZZLE_DATABASE_URL,
+  url: env.DRIZZLE_DATABASE_URL,
 })
 
 export const db = drizzle(client)
