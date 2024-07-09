1cbfdee4d22a8cfb6e90e31d3fea2769640c6e86
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2024-07-09 08:22:14 +0300

    Progress on refactor

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,35 +1,10 @@
 import { type MetaFunction } from '@remix-run/node'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '#app/components/ui/tooltip.tsx'
-import { cn } from '#app/utils/misc.tsx'
-import { logos } from './logos/logos.ts'
 
-export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
-
-// Tailwind Grid cell classes lookup
-const columnClasses: Record<(typeof logos)[number]['column'], string> = {
-  1: 'xl:col-start-1',
-  2: 'xl:col-start-2',
-  3: 'xl:col-start-3',
-  4: 'xl:col-start-4',
-  5: 'xl:col-start-5',
-}
-const rowClasses: Record<(typeof logos)[number]['row'], string> = {
-  1: 'xl:row-start-1',
-  2: 'xl:row-start-2',
-  3: 'xl:row-start-3',
-  4: 'xl:row-start-4',
-  5: 'xl:row-start-5',
-  6: 'xl:row-start-6',
-}
+export const meta: MetaFunction = () => [{ title: 'Mindle' }]
 
 export default function Index() {
   return (
-    <main className="font-poppins grid h-full place-items-center">
+    <main className="font-poppins grid h-full place-items-center bg-background">
       <div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
         <div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
           <a
@@ -68,33 +43,6 @@ export default function Index() {
             file for how to get your project off the ground!
           </p>
         </div>
-        <ul className="mt-16 flex max-w-3xl flex-wrap justify-center gap-2 sm:gap-4 xl:mt-0 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6">
-          <TooltipProvider>
-            {logos.map((logo, i) => (
-              <li
-                key={logo.href}
-                className={cn(
-                  columnClasses[logo.column],
-                  rowClasses[logo.row],
-                  'animate-roll-reveal [animation-fill-mode:backwards]',
-                )}
-                style={{ animationDelay: `${i * 0.07}s` }}
-              >
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <a
-                      href={logo.href}
-                      className="grid size-20 place-items-center rounded-2xl bg-violet-600/10 p-4 transition hover:-rotate-6 hover:bg-violet-600/15 dark:bg-violet-200 dark:hover:bg-violet-100 sm:size-24"
-                    >
-                      <img src={logo.src} alt="" />
-                    </a>
-                  </TooltipTrigger>
-                  <TooltipContent>{logo.alt}</TooltipContent>
-                </Tooltip>
-              </li>
-            ))}
-          </TooltipProvider>
-        </ul>
       </div>
     </main>
   )
