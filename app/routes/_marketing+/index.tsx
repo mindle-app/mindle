efcf0bde3865e76189b57bed52cf06ee1efdee24
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2024-07-09 08:44:42 +0300

    Added mindle logo

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -4,7 +4,7 @@ export const meta: MetaFunction = () => [{ title: 'Mindle' }]
 
 export default function Index() {
   return (
-    <main className="font-poppins grid h-full place-items-center bg-background">
+    <main className="font-poppins grid h-full">
       <div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
         <div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
           <a
