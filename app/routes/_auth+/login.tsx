c5128936ebf0c717a3550e997bb318730991e2b4
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2024-07-10 09:36:11 +0300

    Change name to Mindle

--- app/routes/_auth+/login.tsx
+++ app/routes/_auth+/login.tsx
@@ -199,7 +199,7 @@ export default function LoginPage() {
 }
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Login to Epic Notes' }]
+  return [{ title: 'Login to Mindle' }]
 }
 
 export function ErrorBoundary() {
