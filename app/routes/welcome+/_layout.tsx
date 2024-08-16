7d65a2d7d9fe807cc9a2e13ac4d516e69b7a792f
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2024-08-16 08:50:23 +0300

    Fix move to next question when answer was submitted

--- app/routes/welcome+/_layout.tsx
+++ app/routes/welcome+/_layout.tsx
@@ -1,4 +1,5 @@
 import { Link, Outlet, useSearchParams } from '@remix-run/react'
+import { z } from 'zod'
 import { MindleHead } from '#app/components/illustrations/mindle-head.js'
 import { Icon } from '#app/components/ui/icon.js'
 import { LinkButton } from '#app/components/ui/link-button.js'
@@ -28,28 +29,60 @@ function MessageBubble({
   )
 }
 
+const MultiAnswerSchema = z.array(z.string()).min(1)
+
 export default function WelcomeForm() {
   const [searchParams] = useSearchParams()
   const step = getStep(searchParams)
   const stepNum = Number(step)
-  const question = questions[step]?.question
-
+  const welcomeQuestion = questions[step]
+  const question = welcomeQuestion.question
+  const userAnswer = searchParams.get(`q${step}`)
+  let userCanContinue = step === '5'
+  if (userAnswer) {
+    if (welcomeQuestion.type === 'multi') {
+      const answer = JSON.parse(userAnswer)
+      const result = MultiAnswerSchema.safeParse(answer)
+      userCanContinue = result.success
+    } else {
+      userCanContinue = !!userAnswer
+    }
+  }
   return (
     <div className="container flex h-screen flex-col">
       <div className="flex flex-grow overflow-hidden">
         <main className="items-cetner flex flex-grow flex-col gap-4 overflow-scroll p-5 2xl:p-9">
           <div className="flex flex-col gap-8">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-4">
               <Link
                 to={`/welcome?${withParam(searchParams, 'step', String(step === '1' ? step : Number(step) - 1))}`}
                 aria-disabled={step === '1'}
                 className={cn('flex items-center', {
-                  'opacity-0': step === '1',
+                  'pointer-events-none opacity-0': step === '1',
                 })}
               >
                 <Icon name={'chevron-left'} className="text-foreground" />
               </Link>
               <Progress value={(stepNum / 5) * 100} className="h-2" />
+              <LinkButton
+                to={
+                  step !== '5'
+                    ? `/welcome?${withParam(searchParams, 'step', String(Number(step) + 1))}`
+                    : `/welcome/submit?${searchParams}`
+                }
+                linkProps={{
+                  // https://github.com/remix-run/react-router/issues/1082
+                  className: cn({
+                    'pointer-events-none': !userCanContinue,
+                  }),
+                }}
+                buttonProps={{
+                  variant: 'default',
+                  disabled: !userCanContinue,
+                }}
+              >
+                <Icon name={'chevron-right'} />
+              </LinkButton>
             </div>
             <section className="flex items-center gap-2">
               <MindleHead className="flex-shrink-0" />
@@ -66,8 +99,13 @@ export default function WelcomeForm() {
                   ? `/welcome?${withParam(searchParams, 'step', String(Number(step) + 1))}`
                   : `/welcome/submit?${searchParams}`
               }
+              linkProps={{
+                // https://github.com/remix-run/react-router/issues/1082
+                className: cn({ 'pointer-events-none': !userCanContinue }),
+              }}
               buttonProps={{
                 variant: 'default',
+                disabled: !userCanContinue,
                 size: 'wide',
                 className: 'rounded-full',
               }}
